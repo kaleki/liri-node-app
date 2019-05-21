9198dff104062867d605e2ed706db41f420f981f
kale <intharaksak@gmail.com>
2019-05-21 11:31:20 -0400

    switch case commit

--- liri.js
+++ liri.js
@@ -7,4 +7,26 @@ var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3];
 
+commandInputs(command,input);
 
+function commandInputs (command, input){
+    switch (command){
+        case "concert-this":
+        concertInfo (input);
+        break;
+        
+        case "spotify-this-song":
+        songInfo (input);
+        break;
+
+        case "movie-this":
+        movieInfo (input);
+        break;
+
+        case ("do-what-says"):
+        someInfo (input);
+        break;
+
+        default: console.log("Error. Please try again!")
+    }
+}
