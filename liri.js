db72231b5a075aa09e4048522f46c74ad74b98d1
kale <intharaksak@gmail.com>
2019-05-21 21:33:29 -0400

    final commit for now

--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
+var moment = require("moment")
 var spotify = new Spotify(keys.spotify);
 var command = process.argv[2];
 var input = process.argv[3];
